2642e50dcca0d29cf3a3b8f992a3da4351c86610
GameFlux <admin@gameflux.lol>
2024-10-01 08:53:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://shellshock.io';
+const nggUrl = 'https://www.easyfun.gg/games/roblox.html';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
@@ -11,7 +11,7 @@ const proxy = createProxyMiddleware({
   secure: true,
   logLevel: 'debug',
   router: function(req) {
-    if (req.headers.host === 'shellshock.io) {
+    if (req.headers.host === 'www.easyfun.gg/games/roblox.html) {
       req.headers['X-Forwarded-For'] = ''; 
       req.headers['X-Real-IP'] = '';
       req.headers['Via'] = '';
