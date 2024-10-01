a4083e7a00dab668861751807e2752fab707c220
GameFlux <admin@gameflux.lol>
2024-10-01 09:02:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const proxy = createProxyMiddleware({
   secure: true,
   logLevel: 'debug',
   router: function(req) {
-    if (req.headers.host === 'www.easyfun.gg/games/roblox.html) {
+    if (req.headers.host === 'www.easyfun.gg/games/roblox.html') {
       req.headers['X-Forwarded-For'] = ''; 
       req.headers['X-Real-IP'] = '';
       req.headers['Via'] = '';
